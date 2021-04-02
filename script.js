642309d86334a92971be007822d40f914b81dd73
UĞUR PEKTAŞ <76955236+ugurpektas@users.noreply.github.com>
2021-04-02 19:17:06 +0300

    oluşturulan dizi üzerinden elemanlar sayfaya yansıtıldı

--- script.js
+++ script.js
@@ -4,9 +4,12 @@ const form = document.querySelector('form');
 const input = document.querySelector('#txtTaskName');
 const deleteAllBtn = document.querySelector('#btnDeleteAll');
 const taskList = document.querySelector('#task-list');
+const items = ['item 1','item 2','item 3','item 4'];
 
-
+//load items
+loadItems();
 eventListenners();
+
 
 function eventListenners(){        //formda submit olayı olduğunda , addNewItem fonksiyonu çağırılır.
         //submit eventi
@@ -19,6 +22,32 @@ function eventListenners(){        //formda submit olayı olduğunda , addNewIte
     deleteAllBtn.addEventListener('click', deleteItemAll);
 }
 
+function loadItems() {
+    items.forEach(function(item){
+        createItem(item);
+    })
+}
+
+function createItem(text){
+     // li elamnını oluşturma
+     const li = document.createElement('li');
+     li.className = 'list-group-item list-group-item-secondary';
+         //inputun değerini li'nin içerisine atama
+     li.appendChild(document.createTextNode(text));
+ 
+     //a elemanını oluşturma
+     const a = document.createElement('a');
+     a.className = 'delete-item float-right';
+     a.setAttribute('href','#');
+     a.innerHTML = '<i class="fas fa-times"></i>';
+     
+     // a elemanını li elemanının child'ı olarak atamak
+     li.appendChild(a);
+ 
+     //li elemanını ul'in içerisine atamak
+     taskList.appendChild(li); 
+}
+
     // listeye eleman ekleme fonksiyonu
 function addNewItem(e) {
 
@@ -26,24 +55,9 @@ function addNewItem(e) {
         alert('Değer giriniz.');
     }
 
-    // li elamnını oluşturma
-    const li = document.createElement('li');
-    li.className = 'list-group-item list-group-item-secondary';
-        //inputun değerini li'nin içerisine atama
-    li.appendChild(document.createTextNode(input.value));
-
-    //a elemanını oluşturma
-    const a = document.createElement('a');
-    a.className = 'delete-item float-right';
-    a.setAttribute('href','#');
-    a.innerHTML = '<i class="fas fa-times"></i>';
-    
-    // a elemanını li elemanının child'ı olarak atamak
-    li.appendChild(a);
-
-    //li elemanını ul'in içerisine atamak
-    taskList.appendChild(li);
-
+    //eleman oluşturma
+    createItem(input.value);
+   
     //inputu temizlemek
     input.value = '';
 
@@ -51,13 +65,14 @@ function addNewItem(e) {
     e.preventDefault();
 }
 
-
     //listeden tek eleman silme fonksiyonu
 function deleteItem(e) {
-    
-    if (e.target.className==='fas fa-times') {
-        e.target.parentElement.parentElement.remove();
-    }
+
+        if (e.target.className==='fas fa-times') {
+            if(confirm(`Task'ı silmek istediğinizden emin misiniz?`)){
+            e.target.parentElement.parentElement.remove();
+            }
+        }
     e.preventDefault();
 }
 
@@ -65,12 +80,15 @@ function deleteItem(e) {
 function deleteItemAll(e) {
 
     // taskList.childNodes.forEach(function (item){
-    //     if (item.nodeType===1){   // nodetype element ise bu işlemi yapar
+    //     if (item.nodeType === 1){   // nodetype element ise bu işlemi yapar
     //         item.remove();
     //     }
     // })
+    if (taskList.innerHTML==''){
+        confirm('Task bulunamadı.');
+    }else if (confirm('Tüm taskları silmek istediğinizden emin misiniz?')) {
+        taskList.innerHTML ='';
+    }
     
-    taskList.innerHTML ='';
-
     e.preventDefault();
 }
